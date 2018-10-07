5b1d1bc35bde51e0992e169a97fe7a7a8c8afb11
Pedro Guerrato <pguerrato@gmail.com>
2018-10-06 23:52:08 -0300

    Created about us page

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app.debug = True
 def index():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run()
