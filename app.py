3c3a20efa8566f9ccc28c8aabc49ff885dab5feb
Pedro Guerrato <pguerrato@gmail.com>
2018-10-25 22:39:55 -0300

    Made the Register page layout

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, request, url_for, session, logging
 from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 app.debug = True
@@ -21,6 +24,26 @@ def articles():
 @app.route('/article/<string:id>/')
 def article(id):
     return render_template('article.html', id = id)
+
+class RegisterForm(Form):
+    name = StringField('Name', validators=[validators.Length(min=1, max=256), validators.input_required()])
+    username  = StringField('Username', validators=[validators.Length(min=4, max=256)])
+    email  = StringField('E-mail', validators=[validators.Length(min=6, max=256)])
+    password  = PasswordField('Password', validators=[
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match'),
+        validators.Length(min=6, max=256)
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+
+    if (request.method == 'POST' and form.validate):
+        pass
+    
+    return render_template('register.html', form=form)
 
 if __name__ == '__main__':
     app.run()
